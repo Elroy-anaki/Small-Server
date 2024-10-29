db40fd7392e26777d66c8be44155ee8c5c21bab1
Elroy Anaki <elroy.a1912@gmail.com>
2024-10-29 23:39:49 +0200

    Create a DB + work with the 'fs' module

--- app.js
+++ app.js
@@ -1,15 +1,25 @@
 // Import the modules
-const express = require('express');
-const env = require('dotenv');
+const express = require("express");
+const env = require("dotenv");
+const {
+  getAllUsers,
+  deleteUser,
+  addUser,
+  updateUser,
+} = require("./Actions/CRUD");
+let { usersList } = require("./Actions/CRUD");
 
-// Define the app and the port
+// Define the app, the port and the database path
 const app = express();
-const port = Number(env.config().parsed.PORT)
-
-
+const PORT = Number(env.config().parsed.PORT);
+const DB_PATH = String(env.config().parsed.DB_PATH);
 
 // The GET requests
-app.get('/', (req, res) => res.send('Hello World!'))
+app.get("/", (req, res) => res.send(`Hello World!${DB_PATH}`));
 
+app.get("/users", (req, res) => {
+  usersList = getAllUsers(DB_PATH);
+  res.send(usersList);
+});
 
-app.listen(port, () => console.log(`Server runs on port ${port} ...`))
+app.listen(PORT, () => console.log(`Server runs on port ${PORT} ...`));
